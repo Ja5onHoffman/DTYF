f745788a3f673dec831fcd806198dfa10c83994e
Jason <j@sonhoffman.com>
2024-02-12 13:48:32 -0600

    Initial Commit

---
+++ DTYF/DTYFApp.swift
@@ -0,0 +1,17 @@
+//
+//  DTYFApp.swift
+//  DTYF
+//
+//  Created by Jason Hoffman on 2/12/24.
+//
+
+import SwiftUI
+
+@main
+struct DTYFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
